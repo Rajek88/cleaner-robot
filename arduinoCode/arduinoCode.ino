1b177ad34806ba89763385c01b4a51de896286be
Rajendra Kulkarni <rajerkulkarni01@gmail.com>
2022-01-01 22:32:24 +0530

    Updated Arduino code

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,22 +1,13 @@
 //FirebaseESP8266.h must be included before ESP8266WiFi.h
 #include "FirebaseESP8266.h"  // Install Firebase ESP8266 library
 #include <ESP8266WiFi.h>
-//#include <DHT.h>    // Install DHT11 Library and Adafruit Unified Sensor Library
 
 
-
-// apiKey: "AIzaSyBB29Bzt39YAZpaNhvXTJywGIzw7_fWaeg",
-//  authDomain: "stage-cleaner.firebaseapp.com",
 #define FIREBASE_HOST "stage-cleaner-default-rtdb.firebaseio.com" //Without http:// or https:// schemes
 #define FIREBASE_AUTH "xdBy3YYyyL509HLeqT9tWbifppDsLRaN1eUW0Ki4"
 #define WIFI_SSID "Mukund_Speed"
 #define WIFI_PASSWORD "Five@Mitsoe"
 
-//#define DHTPIN 2    // Connect Data pin of DHT to D2
-//int led = D5;     // Connect LED to D5
-
-//#define DHTTYPE    DHT11
-//DHT dht(DHTPIN, DHTTYPE);
 
 //Define FirebaseESP8266 data object
 FirebaseData firebaseData;
@@ -24,14 +15,90 @@ FirebaseData ledData;
 
 FirebaseJson json;
 
+#define ENA   14          // Enable/speed motors Right        GPIO14(D5)
+#define ENB   12          // Enable/speed motors Left         GPIO12(D6)
+#define IN_1  15          // L298N in1 motors Rightx          GPIO15(D8)
+#define IN_2  13          // L298N in2 motors Right           GPIO13(D7)
+#define IN_3  2           // L298N in3 motors Left            GPIO2(D4)
+#define IN_4  0           // L298N in4 motors Left            GPIO0(D3)
+//int motor1pin1 = 15;
+//int motor1pin2 = 13;
+//
+//int motor2pin1 = 2;
+//int motor2pin2 = 0;
+
+int speedCar = 950;         // 400 - 1023.
+int speed_Coeff = 3;
+
+void applyBreak(){
+        digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+}
+
+void goAhead(){ 
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goBack(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goRight(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goLeft(){
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
 
 void setup()
 {
 
   Serial.begin(9600);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT); 
+//  pinMode(motor1pin1, OUTPUT);
+//  pinMode(motor1pin2, OUTPUT);
+//  pinMode(motor2pin1, OUTPUT);
+//  pinMode(motor2pin2, OUTPUT);
+   pinMode(IN_1, OUTPUT);
+   pinMode(IN_2, OUTPUT);
+   pinMode(IN_3, OUTPUT);
+   pinMode(IN_4, OUTPUT);
 
-//  dht.begin();
-//  pinMode(led,OUTPUT);
   
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -50,83 +117,43 @@ void setup()
 
 }
 
-//void sensorUpdate(){
-//  // Reading temperature or humidity takes about 250 milliseconds!
-//  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-////  float h = dht.readHumidity();
-//  // Read temperature as Celsius (the default)
-////  float t = dht.readTemperature();
-//  // Read temperature as Fahrenheit (isFahrenheit = true)
-////  float f = dht.readTemperature(true);
-//
-//  // Check if any reads failed and exit early (to try again).
-//  if (isnan(h) || isnan(t) || isnan(f)) {
-//    Serial.println(F("Failed to read from DHT sensor!"));
-//    return;
-//  }
-//
-//  Serial.print(F("Humidity: "));
-//  Serial.print(h);
-//  Serial.print(F("%  Temperature: "));
-//  Serial.print(t);
-//  Serial.print(F("C  ,"));
-//  Serial.print(f);
-//  Serial.println(F("F  "));
-//
-//  if (Firebase.setFloat(firebaseData, "/FirebaseIOT/temperature", t))
-//  {
-//    Serial.println("PASSED");
-//    Serial.println("PATH: " + firebaseData.dataPath());
-//    Serial.println("TYPE: " + firebaseData.dataType());
-//    Serial.println("ETag: " + firebaseData.ETag());
-//    Serial.println("------------------------------------");
-//    Serial.println();
-//  }
-//  else
-//  {
-//    Serial.println("FAILED");
-//    Serial.println("REASON: " + firebaseData.errorReason());
-//    Serial.println("------------------------------------");
-//    Serial.println();
-//  }
-//
-//  if (Firebase.setFloat(firebaseData, "/FirebaseIOT/humidity", h))
-//  {
-//    Serial.println("PASSED");
-//    Serial.println("PATH: " + firebaseData.dataPath());
-//    Serial.println("TYPE: " + firebaseData.dataType());
-//    Serial.println("ETag: " + firebaseData.ETag());
-//    Serial.println("------------------------------------");
-//    Serial.println();
-//  }
-//  else
-//  {
-//    Serial.println("FAILED");
-//    Serial.println("REASON: " + firebaseData.errorReason());
-//    Serial.println("------------------------------------");
-//    Serial.println();
-//  }
-//}
+
 void loop() {
-//  sensorUpdate();
   
   if (Firebase.getString(ledData, "/x/cmd")){
-    Serial.println(ledData.stringData());
-    if (ledData.stringData() == "left") {
+//    Serial.println(ledData.stringData());
+    String cmd =  ledData.stringData() ;
+//    String left = "left1";
+//    cmd.trim();
+    Serial.println(cmd);
+    
+    if (cmd == "\"left\"") {
+//      Serial.println("LEFT");
 //    digitalWrite(led, HIGH);
+//        digitalWrite(motor1pin1, HIGH);
+//        digitalWrite(motor1pin2, LOW);
+//        digitalWrite(motor2pin1, HIGH);
+//        digitalWrite(motor2pin2, LOW);
+      goLeft();
       Serial.println("LEFT");
     }
-  else if (ledData.stringData() == "right"){
+  else if (ledData.stringData() == "\"right\""){
 //    digitalWrite(led, LOW);
+      goRight();
       Serial.println("RIGHT");
     }
-  else if (ledData.stringData() == "forward"){
+  else if (ledData.stringData() == "\"forward\""){
 //    digitalWrite(led, LOW);
+      goAhead();
       Serial.println("FORWARD");
     }
-  else if (ledData.stringData() == "backward"){
+  else if (ledData.stringData() == "\"backward\""){
 //    digitalWrite(led, LOW);
+      goBack();
       Serial.println("BACKWARD");
+    }
+    else{
+      applyBreak();
     }
   }
   delay(100);
